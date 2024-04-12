9e181785fe2cc14f51bb5663eb532a28b7d9268b
Roy Vanegas <roy@thecodeeducators.com>
2024-04-12 10:53:09 -0400

    Add Bryant Ortiz’s assignment by hand. No penalty

--- js/main.js
+++ js/main.js
@@ -1,21 +1,47 @@
 window.onload = () => {
-       alert('Test');                    
-}
+    alert(`Test`);
+};
+// Place the styles
+// 1.) Weights of 300 & 600
+// 2.) Pixel size = 16 px.
+// 3.) Content Leading = 1.4
+// 4.) font size of h1 = 32 px.
+// 5.) leading of h1 element = 38 px.
+// 6.) width of each slide = 640 px
+// --------------> Left/Right padding = 20 px.
 
+//Editing the Weights:
+window.onload =() =>{
+    Header = document.querySelector('h1');
+    body = document.querySelector('body');
+    document.innerHTML = <div><section></section></div>;
+    document.innerHTML= <div class carousel-slides></div>;
+    document.images.
+};
+//Event listener
+body.addeventlistener('Click',()=>{
+    body.style.backgroundcolor='yellow';
 
+})
+let toggle = true;
+body.addeventlistener('Click',()=>{
+    if(toggle){
+        body.style.backgroundcolor='yellow';
+        toggle = 'false';
+    }else{
+        body.style.backgroundcolor='transparent';
+        toggle='false';
+    }
+})
+imageOne = document.createElement('imageOne')
+imageOne.setAttribute('src','img/n');
+imageTwo = document.createElement('imageTwo')
+imageTwo.setAttribute('src','');
+imageThree = document.createElement('imageThree')
+imageThree.setAttribute('src','');
+imageFour = document.createElement('imageFour')
+imageFour.setAttribute('src','');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+script = document.createElement(`script`);
+script.setAttribute(`src`, `data/data.json`);
+body.appendChild(script);
